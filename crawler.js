798ed561d2c34e741de9023082ee42082b7f1039
Vincent Zhu <CN1463@cn.melaleuca.net>
2016-08-24 17:20:26 +0800

    NodejsPlayaround

---
+++ crawler.js
@@ -0,0 +1,33 @@
+var http = require('http')
+var cheerio = require('cheerio')
+var url = 'http://www.melaleuca.com.cn'
+
+function filterChapters(html){
+	var $ = cheerio.load(html)
+	var chapters = $('.learnchapter')
+	//var chapter = 
+	/*[{
+		chapterTitle: '',
+		videos:[
+			title:'',
+			id:''
+		]
+	}]*/
+
+
+}
+
+http.get(url,function(res){
+	var html =''
+
+	res.on('data',function(data){
+		html += data
+	})
+
+	res.on('end',function(){
+		//filterChapters(html)
+		console.log(html)
+	})
+}).on('error',function(e){
+	console.log(e.message)
+})
