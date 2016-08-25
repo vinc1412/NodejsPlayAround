9bb1c75879d683deb5c993a99b9a71a383b73fe6
vincent <vincent.z@msnc.com>
2016-08-25 15:10:24 +0800

    parse title

--- crawler.js
+++ crawler.js
@@ -1,10 +1,31 @@
 var http = require('http')
 var cheerio = require('cheerio')
-var url = 'http://www.melaleuca.com.cn'
+var url = 'http://www.imooc.com/learn/348'
+
+http.get(url,function(res){
+	var html =''
+
+	
+	res.on('data',function(data){
+		html += data
+	})
+
+	res.on('end',function(){
+		var courseData = filterChapters(html)
+		printCourseData(courseData)
+		//console.log(courseData)
+	})
+}).on('error',function(e){
+	console.log(e.message)
+})
+
+
 
 function filterChapters(html){
 	var $ = cheerio.load(html)
-	var chapters = $('.learnchapter')
+	var courseData = []
+	var chapters = $('.chapter')
+	
 	//var chapter = 
 	/*[{
 		chapterTitle: '',
@@ -14,21 +35,39 @@ function filterChapters(html){
 		]
 	}]*/
 
-	var test=''
+	chapters.each(function(item){
+
+		var chapter = $(this)
+		var chapterTitle = chapter.find('strong').text().replace(/\s+/g,'')
+		var videos = chapter.find('.video').children('li')
+		var chapterData ={
+			chapterTitle: chapterTitle,
+			videos: []
+		}
+
+		videos.each(function(item){
+			var video = $(this)
+			var videoTitle = video.find('.J-media-item').text().replace(/\s+/g,'')
+			var id =  video.attr('data-media-id')
+
+
+			chapterData.videos.push({
+				title: videoTitle,
+				id: id
+			})
+		})
+		courseData.push(chapterData)
+	})
+	return courseData
 }
 
-http.get(url,function(res){
-	var html =''
+function printCourseData(courseData){
+	courseData.forEach(function(item){
+		console.log(item.chapterTitle + '\n')
 
-	res.on('data',function(data){
-		html += data
+		item.videos.forEach(function(video){
+			console.log('  ['+video.id +'] ' + video.title +'\n')
+		})
 	})
-
-	res.on('end',function(){
-		//filterChapters(html)
-		console.log(html)
-	})
-}).on('error',function(e){
-	console.log(e.message)
-})
+}
 
